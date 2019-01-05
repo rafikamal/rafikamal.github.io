28fca4a3d24af717d4a6993d71b9a2363992ebae
rafikamal <rafikamal10788@users.noreply.github.com>
2019-01-05 18:39:09 +0700

    UTS_A11_2017_10788_MuhammadRafiKamal

---
+++ js/button.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  $(".btn-primary").mouseover(function(){
+    var div = $(".btn-primary");
+    div.animate({width: '560px', opacity: '0.8'}, "slow");
+    div.animate({width: '140px', opacity: '0.8'}, "slow");
+    div.css({"text-align":"right"});
+  });
+});
+$(document).ready(function(){
+    $(".btn-dark").mouseover(function(){
+      var div = $(".btn-dark");
+      div.animate({width: '560px', opacity: '0.8'}, "slow");
+      div.animate({width: '140px', opacity: '0.8'}, "slow");
+      div.css({"text-align":"right"});
+    });
+  });
+  $(document).ready(function(){
+    $(".btn-info").mouseover(function(){
+      var div = $(".btn-info");
+      div.animate({width: '560px', opacity: '0.8'}, "slow");
+      div.animate({width: '140px', opacity: '0.8'}, "slow");
+      div.css({"text-align":"right"});
+    });
+  });
+  $(document).ready(function(){
+    $(".btn-secondary").mouseover(function(){
+      var div = $(".btn-secondary");
+      div.animate({width: '560px', opacity: '0.8'}, "slow");
+      div.animate({width: '140px', opacity: '0.8'}, "slow");
+      div.css({"text-align":"right"});
+    });
+  });
+  $(document).ready(function(){
+    $(".btn-success").mouseover(function(){
+      var div = $(".btn-success");
+      div.animate({width: '560px', opacity: '0.8'}, "slow");
+      div.animate({width: '140px', opacity: '0.8'}, "slow");
+      div.css({"text-align":"right"});
+    });
+  });
